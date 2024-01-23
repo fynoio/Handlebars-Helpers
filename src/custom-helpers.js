66621cbf4eebce9892e0bec4a57a648da95a8326
pranav badami <pranav@fyno.io>
2024-01-23 10:03:05 +0000

    Resolve "Testing Build with Frontend"

--- src/custom-helpers.js
+++ src/custom-helpers.js
@@ -172,6 +172,10 @@ handlebars.registerHelper('isJSON', function (context) {
     }
 })
 
+handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+    return arg1 == arg2 ? options.fn(this) : options.inverse(this)
+})
+
 handlebars.registerHelper('stringify', function (context) {
     return JSON.stringify(context)
 })
